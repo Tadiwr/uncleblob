32adfcf3ec23f33de97c87c48991c4e609f08a5f
Tadiwanashe Shangwa <tshxng@gmail.com>
2024-11-07 01:42:52 +0200

    refactors code by adding function to get the name of the blob store to ensure consistency

--- src/providers/storage/utils.rs
+++ src/providers/storage/utils.rs
@@ -4,10 +4,14 @@ use crate::providers::utils::gitignore::add_to_gitignore;
 
 pub fn initialise_store() {
 
-    let store_path = ".blobstore";
+    let store_path = get_store_path();
 
     if !fs::exists(&store_path).unwrap() {
         fs::create_dir(&store_path).unwrap();
         add_to_gitignore(&store_path);
     }
+}
+
+pub fn get_store_path() -> String {
+    return ".blobstore".to_string();
 }
