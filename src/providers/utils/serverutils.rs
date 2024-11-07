91a91149d7a209922307a42b327fceaf0aee79fb
Tadiwanashe Shangwa <tshxng@gmail.com>
2024-11-07 02:05:57 +0200

    adds code to get server address and port

---
+++ src/providers/utils/serverutils.rs
@@ -0,0 +1,9 @@
+/** Returns the address of the server */
+pub fn get_server_address() -> String {
+    return "127.0.0.1".to_string();
+}
+
+/** Returns the PORT the server is listening to */
+pub fn get_server_port() -> u32 {
+    return 5050;
+}
