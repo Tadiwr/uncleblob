7aa4da4e457cc87e2f85c3213bbf1888080a01cc
Tadiwanashe Shangwa <tshxng@gmail.com>
2024-11-07 21:40:30 +0200

    adds apis for overwriting existing files and delete files

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use actix_web::{get, post, web, App, HttpResponse, HttpServer, Responder};
+use actix_web::{delete, get, post, put, web::{self, PayloadConfig}, App, HttpResponse, HttpServer, Responder};
 use providers::{storage, utils::serverutils::{get_server_address, get_server_port}};
 
 mod providers;
@@ -10,13 +10,43 @@ async fn index() -> impl Responder {
 }
 
 
+/** Route to upload a file */
 #[post("/storage/upload/{bucket_name}/{file_name}")] 
-async fn upload(path: web::Path<(String, String)>, payload: web::Bytes) -> impl Responder {
+async fn upload_file(path: web::Path<(String, String)>, payload: web::Bytes) -> impl Responder {
 
     let (bucket_name, file_name) = path.into_inner();
     let buffer = payload.to_vec();
 
     let res = storage::api::put(
+        file_name.as_str(),
+        bucket_name.as_str(),
+        buffer
+    );
+
+    match res {
+        Ok(blob) => {
+            return HttpResponse::Ok()
+            .append_header(("Content-Type", "application/json"))
+            .body(blob.to_json());
+        }
+
+        Err(err) => {
+
+            // FIXME: Server is responding with wrong error code
+            return HttpResponse::NotFound().body(err);
+        }
+    }
+
+}
+
+/** Route to update or overwrite an existing file */
+#[put("/storage/upload/{bucket_name}/{file_name}")] 
+async fn overwrite_file(path: web::Path<(String, String)>, payload: web::Bytes) -> impl Responder {
+
+    let (bucket_name, file_name) = path.into_inner();
+    let buffer = payload.to_vec();
+
+    let res = storage::api::overwrite(
         file_name.as_str(),
         bucket_name.as_str(),
         buffer
@@ -36,6 +66,7 @@ async fn upload(path: web::Path<(String, String)>, payload: web::Bytes) -> impl 
 
 }
 
+/** Route to get an existing file */
 #[get("/storage/{bucket_name}/{file_name}")] 
 async fn retrieve_file(path: web::Path<(String, String)>) -> impl Responder {
 
@@ -54,12 +85,25 @@ async fn retrieve_file(path: web::Path<(String, String)>) -> impl Responder {
     }
 }
 
+#[delete("/storage/delete/{bucket_name}/{file_name}")] 
+async fn delete_file(path: web::Path<(String, String)>) -> impl Responder {
+
+    let (bucket_name, file_name) = path.into_inner();
+    
+    storage::api::delete(file_name.as_str(), bucket_name.as_str());
+
+    return  HttpResponse::Ok();
+}
+
 
 #[actix_web::main]
 async fn main() -> Result<(), std::io::Error> {
 
     let addr = get_server_address();
     let port = get_server_port();
+
+    // Sets upload limit to 10 mebi bytes
+    const UPLOAD_LIMIT: usize = 10 * 1024 * 1024;
 
     println!("\n-------------------------------------------------------------\n");
     println!("Server running at http://{}:{}", addr, port);
@@ -69,7 +113,13 @@ async fn main() -> Result<(), std::io::Error> {
         App::new()
         .service(index)
         .service(retrieve_file)
-        .service(upload)
+        .service(upload_file)
+        .service(delete_file)
+        .service(overwrite_file)
+        .app_data(
+            // Upload limit here
+            PayloadConfig::new(UPLOAD_LIMIT)
+        )
     })
         .bind((addr, port)).unwrap()
         .run()
--- src/providers/storage/api.rs
+++ src/providers/storage/api.rs
@@ -79,3 +79,47 @@ pub fn put(file_name: &str, bucket_name: &str, buffer: Vec<u8>)  -> Result<Blob,
         );
     }
 }
+
+
+
+/** Deletes a file with the specified name and storage bucket */
+pub fn delete(file_name: &str, bucket_name: &str) {
+
+    initialise_store();
+    
+    let store_path = get_store_path();
+    let file_path = format!("{}/{}::{}", store_path, bucket_name, file_name);
+
+    if fs::exists(&file_path).unwrap() {
+        fs::remove_file(file_path).unwrap();
+    }
+}
+
+
+/** Overides the contents of an existing file */
+pub fn overwrite(file_name: &str, bucket_name: &str, buffer: Vec<u8>)  -> Result<Blob, String>{
+
+    initialise_store();
+    
+    let store_path = get_store_path();
+    let file_path = format!("{}/{}::{}", &store_path, &bucket_name, &file_name);
+
+    let file_result = File::options()
+        .create(true)
+        .write(true)
+        .open(&file_path);
+
+    match file_result {
+        Ok(mut file) => {
+            file.write_all(&buffer).unwrap();
+
+            return Ok(Blob::new(file_name.to_string(), bucket_name.to_string(), buffer));
+        }
+
+        Err(_) => {
+            return  Err(String::from("Error occured whiles overwriting file"));
+        }
+    }
+
+}
+
